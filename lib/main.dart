183d3220ff2316971c190bfe32c9c61126030a87
Valeria Chavira <ChaviraVa@outlook.com>
2025-07-08 14:32:16 -0700

    Colores y app

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:proyectomanu/app.dart';
 import 'package:proyectomanu/utils/theme/theme.dart';
 
 void main() {
   runApp(const App());
-}
-
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      themeMode: ThemeMode.system,
-      theme: TAppTheme.lightTheme,
-      darkTheme: TAppTheme.darkTheme,
-    );
-  }
 }
---
+++ lib/utils/constants/colors.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class TColors {
+  TColors._();
+  //Colores de la aplicacion
+  static const Color primaryColor = Color(0Xff0073e6);
+  static const Color secondaryColor = Color(0Xff00b4c5);
+  static const Color teciaryColor = Color(0xff2546f0);
+
+  //texto
+  static const Color textPrimary = Color.fromARGB(255, 0, 0, 0);
+  static const Color textSecondary = Color(0xffb3c7f7);
+  static const Color textWhite = Colors.white;
+
+  //BackgroundColors
+  static const Color light = Colors.white;
+  static const Color dark = Color.fromARGB(255, 0, 0, 0);
+  static const Color primaryBackground = Color(0xffb3c7f7);
+
+  //botones
+  static const Color defaultBoton = Colors.white;
+  static const Color primarioBoton = Color(0xff38bff9);
+  static const Color secundarioBoton = Color(0xff24c460);
+  static const Color peligroBoton = Color(0xfff63f5e);
+  static const Color superBoton = Color(0xff6064ec);
+  static const Color fantasmaBoton = Color(0xff74757a);
+
+  static const Color intermedioAzul = Color(0xffd9e4ff);
+  static const Color intermediofuerteAzul = Color(0xff8babf1);
+}
